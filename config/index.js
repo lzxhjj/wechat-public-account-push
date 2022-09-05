5a0283ccf8c4d1c7e5b63d9626a8b0a45a448663
lzxhjj <65992228+lzxhjj@users.noreply.github.com>
2022-09-05 21:49:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -15,31 +15,31 @@ export const config = {
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
+   * //[
+   * // {
    *    // 想要发送的人的名字
-   *    name: "老婆0",
+   *  //  name: "佳佳",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *  //  id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   * //   useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
+   *  //  openUrl: "https://wangxinleo.cn"
+   *  // }, 
+   * //]
    */
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "佳佳",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "omdNB509gtzp_KdXXcmzA6N9k_Ow",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "wx893e804937d2b647",
       // 所在省份
-      province: "广东",
+      province: "河南",
       // 所在城市
-      city: "肇庆",
+      city: "南阳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -80,9 +80,9 @@ export const config = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "土豆",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "omdNB509gtzp_KdXXcmzA6N9k_Ow",
     }, 
   ],
     
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "河南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "南阳",
 
   /** 重要节日相关 */
 
@@ -106,10 +106,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "最可爱的佳佳", "year": "1998", "date": "12-01"},
+    {"type": "节日", "name": "相恋", "year": "2018", "date": "06-25"},
+    {"type": "生日", "name": "最赚钱的志志", "year": "1997", "date": "01-13"},
+    {"type": "节日", "name": "相识", "year": "2018", "date": "06-04"},
   ],
 
   /**
@@ -127,11 +127,11 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+    {"keyword": "相识", date: "2018-06-04"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+    {"keyword": "相恋", date: "2018-06-25"},
     // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    //{"keyword": "ex_day", date: "2022-08-31"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
@@ -145,13 +145,24 @@ export const config = {
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
+    //{"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
       "因为太喜欢你，所以看谁都像是情敌。",
       "申请成为你爱里的永久居民。",
       "你很傻，你很笨，可我还是很羡慕你，因为你有我",
       "遇见你，就好像捡到了100斤的运气",
+      "我不知道我还会再遇到谁，我也不知道我将要面对谁，但是我一看到你就觉得人间值得。"
+"你知道什么叫意外吗？就是我从没想过会遇见你，但我遇见了，我从没想过会爱你，但我爱了。"
+"如果想念有声音，恐怕早已震耳欲聋。"
+"无论发生什么事，我都会像现在一样爱你，直到永远。"
+"爱你就是这么莫名其妙，就是这么义无返顾，我知道我不会是你今生的唯一，但你却是我一生的最爱！"
+"开心的时候想亲你，不开心的时候想被你亲。"
+"我爱你，不是因为你是一个怎样的人，而是因为我喜欢与你在一起时的感觉。"
+"我们的爱情，希望一直没有句号"
+"从前，眼前人是心上人。以后，心上人是枕边人。"
+"喜欢你，像云漂泊九万里，不曾歇息。喜欢你，像风走了八千里，不问归期。"
+
     ]},
   ],
 
