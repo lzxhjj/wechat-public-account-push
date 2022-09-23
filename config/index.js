ab761f6d2fe1d74073384434dfd16ff274f6040f
lzxhjj <65992228+lzxhjj@users.noreply.github.com>
2022-09-23 20:00:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,33 +1,4 @@
 export const config = {
-  /**
-   * 公众号配置
-   */
-
-  // 公众号APP_ID 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  // APP_ID: "",
-
-  // 公众号APP_SECRET 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  // APP_SECRET: "",
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "佳佳",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *   id: "omdNB5yfc2xR0uMN4x5fYi429-yY",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "wx893e804937d2b647",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *   openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
-   */
   USERS: [
     {
       // 想要发送的人的名字
@@ -49,22 +20,6 @@ export const config = {
       useTemplateId: "wx893e804937d2b647",
       province: "河南",
       city: "南阳",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆3", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
       openUrl: "https://wangxinleo.cn"
     }, 
   ],
